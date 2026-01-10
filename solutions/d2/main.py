897a3dea055f3f0e8c8089cf1e0187fd51babad9
jacerobinson21@gmail.com <12394543+JaceRobinson8@users.noreply.github.com>
2026-01-10 11:02:31 -0500

    sol day 2

--- solutions/d2/main.py
+++ solutions/d2/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from pathlib import Path
 from dataclasses import dataclass
+from itertools import batched
 
 
 @dataclass(frozen=True)
@@ -23,37 +24,45 @@ def parse_file(file_path: str | Path) -> list[IDRange]:
         ]
 
 
-def get_invalid_idx_from_ranges(idx_ranges: list[IDRange]) -> list[int]:
+def get_invalid_idx_from_ranges(
+    idx_ranges: list[IDRange], num_repeats: int = -1
+) -> list[int]:
     result: list[int] = []
     for idx_range in idx_ranges:
-        result.extend(get_invalid_idx_from_range(idx_range))
+        result.extend(get_invalid_idx_from_range(idx_range, num_repeats))
     return result
 
 
-def get_invalid_idx_from_range(idx_range: IDRange) -> list[int]:
+def get_invalid_idx_from_range(idx_range: IDRange, num_repeats: int = -1) -> list[int]:
     return [
         idx
         for idx in range(idx_range.start, idx_range.end + 1)
-        if is_invalid_id(str(idx))
+        if is_invalid_id(str(idx), num_repeats)
     ]
 
 
-def is_invalid_id(idx: str) -> bool:
-    # Invalid id repeats a sequence
-    id_length = len(idx)
+def is_invalid_id(idx: str, num_repeats: int = -1) -> bool:
+    """num_repeats = -1 check for repeat at least twice.
 
-    if (len(idx) % 2) == 1:  # odd length sequences won't be invalid
-        return False
+    Otherwise check exactly num_repeats.
+    """
+    invalid = False
+    if num_repeats == -1:  # no length specified, try every combination
+        for i in range(len(idx) - 1):
+            if len(set(batched(idx, i + 1))) == 1:
+                invalid = True
+                break
     else:
-        return idx[: int(id_length / 2)] == idx[int(id_length / 2) :]
+        batch_size = max(int(len(idx) / num_repeats), 2)
+        invalid = len(set(batched(idx, batch_size))) == 1
+    return invalid
 
 
 def main():
     BASE_DIR = Path(__file__).resolve().parent
     idx_ranges = parse_file(BASE_DIR / "input1.txt")
-    print(f"Answer (p1): {sum(get_invalid_idx_from_ranges(idx_ranges))}")
-
-    # print(f"Answer (p2): {count_0s_p2(rotations=rotations)}")
+    print(f"Answer (p1): {sum(get_invalid_idx_from_ranges(idx_ranges, 2))}")
+    print(f"Answer (p2): {sum(get_invalid_idx_from_ranges(idx_ranges, -1))}")
 
 
 if __name__ == "__main__":
