2f4585dd8233a17ab7ffd42dd50aa903ba884c65
jacerobinson21@gmail.com <12394543+JaceRobinson8@users.noreply.github.com>
2026-01-10 13:40:45 -0500

    correct day3 p2

--- solutions/d3/main.py
+++ solutions/d3/main.py
@@ -12,39 +12,28 @@ class Bank:
         return cls([int(x) for x in batteries])
 
 
-def get_bank_joltage_p1(bank: Bank) -> int:
-    # First get max value.
-    # Then get max value to right of previous max
-    # This produces the largest joltage
-    # Corner case, first max can't be last digit
-    max_index, max_val = max(enumerate(bank.batteries[:-1]), key=lambda x: x[1])
-    max2_index, max2_val = max(
-        enumerate(bank.batteries[(max_index + 1) :]), key=lambda x: x[1]
-    )
-    return int(str(max_val) + str(max2_val))
-
-
-def get_bank_joltage_p2(bank: Bank, batteries_to_turn_on: int) -> int:
-    batteries = []
-    max_idx = -1
+def get_bank_joltage(bank: Bank, batteries_to_turn_on: int) -> int:
+    active_battery_joltage: list = []
+    max_idx: int = -1
     total_batteries = len(bank.batteries)
     for idx in range(batteries_to_turn_on):
         max_idx, max_val = get_max_battery_in_slice(
             bank.batteries,
-            range_start=max_idx + 1,
-            range_end=total_batteries - batteries_to_turn_on + idx + 1,
+            start=max_idx + 1,
+            end=total_batteries - batteries_to_turn_on + 1 + idx,
         )
-        batteries.append(str(max_val))
-    return int("".join(batteries))
+        active_battery_joltage.append(str(max_val))
+    return int("".join(active_battery_joltage))
 
 
 def get_max_battery_in_slice(
-    batteries: list[int], range_start: int, range_end: int
+    batteries: list[int], start: int, end: int
 ) -> tuple[int, int]:
-    return max(
-        enumerate(batteries[range_start:range_end]),
+    max_idx, max_val = max(
+        enumerate(batteries[start:end]),
         key=lambda x: x[1],
     )
+    return max_idx + start, max_val
 
 
 def parse_file(file_path: str | Path) -> list[Bank]:
@@ -55,9 +44,9 @@ def parse_file(file_path: str | Path) -> list[Bank]:
 
 def main():
     BASE_DIR = Path(__file__).resolve().parent
-    banks = parse_file(BASE_DIR / "input_sample.txt")
-    print(f"Answer (p1): {sum([get_bank_joltage_p1(bank) for bank in banks])}")
-    print(f"Answer (p2): {sum([get_bank_joltage_p2(bank, 12) for bank in banks])}")
+    banks = parse_file(BASE_DIR / "input1.txt")
+    print(f"Answer (p1): {sum([get_bank_joltage(bank, 2) for bank in banks])}")
+    print(f"Answer (p2): {sum([get_bank_joltage(bank, 12) for bank in banks])}")
 
 
 if __name__ == "__main__":
